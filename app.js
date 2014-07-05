cd1c679ae95023ec967560c51a47d30c83e5d4f6
Joe Whitfield-Seed <joe.ws@ripjar.com>
2014-07-05 14:33:31 +0100

    Remove unused boilerplate, single-user passport.js authentication

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    passport = require('passport'),
+    BasicStrategy = require('passport-http').BasicStrategy;
+
+// Tmp - authenticate against single hard coded user
+function authenticate(username, password, done) {
+  if(!username) {
+    done(null, false);
+  } else if(username === 'user' && password === 'password') {
+    done(null, { username: 'user' });
+  } else {
+    done(null, null);
+  }
+}
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
+
 app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(passport.initialize());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.errorHandler());
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+passport.use(new BasicStrategy({}, authenticate));
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// curl -i --user user:password localhost:3000
+app.get('/',
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+   res.json(req.user);
+  });
 
-http.createServer(app).listen(app.get('port'), function(){
+
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
